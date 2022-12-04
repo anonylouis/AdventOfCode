63c0b6eb4ce80b7805d40294a547289435bda0d3
anonylouis <louis.calvie@gmail.com>
2022-12-04 13:32:47 +0100

    day0 4

---
+++ day04/day4_part1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+f = open('input.txt')
+
+s = 0
+for l in f :
+	l = l[:-1]
+	elves1, elves2 = l.split(",")
+	elves1 = list(map(int, elves1.split("-")))
+	elves2 = list(map(int, elves2.split("-")))
+	if (elves1[0] <= elves2[0] and elves1[1] >= elves2[1]) or (elves2[0] <= elves1[0] and elves2[1] >= elves1[1]) :
+		s+=1
+
+print(s)
