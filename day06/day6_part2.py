a4892fbe3a56882de7d387cf564cb2e606ed2d8f
Louis CALVIE <lcalvie@student.42.fr>
2022-12-06 10:38:58 +0100

    day6

---
+++ day06/day6_part2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+f = open('input.txt')
+
+l = f.readline()
+for i in range(4, len(l)) :
+	L = set([l[j] for j in range(i - 13, i +1)])
+	if (len(L)  == 14) :
+		print(i + 1)
+		break
