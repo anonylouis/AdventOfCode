79dc12ae50994ec2cbda0455152ef66112a416aa
Louis CALVIE <lcalvie@student.42.fr>
2022-12-15 15:50:07 +0100

    day15

--- day15/day15_part1.py
+++ day15/day15_part1.py
@@ -2,49 +2,37 @@
 
 f = open('input.txt')
 
-xmin, xmax, ymin, ymax = -1, -1, -1, -1
-C=[]
+BS = []
+P  = []
+y_search = 2000000
 
 for l in f :
 	L=l[:-1].replace("=", " ").replace(",", " ").replace(":", " ").split()
 	L = list(map(int, [L[3], L[5], L[11], L[13]]))
-	L.append(abs(L[0] - L[2]) + abs(L[1] - L[3]))
-	C.append(L)
-	X, Y = [L[0] - L[4], L[0] + L[4]], [L[1] - L[4], L[1] + L[4]]
-	xmin = min(X) if xmin==-1 or min(X) < xmin else xmin
-	xmax = max(X) if xmax==-1 or max(X) > xmax else xmax
-	ymin = min(Y) if ymin==-1 or min(Y) < ymin else ymin
-	ymax = max(Y) if ymax==-1 or max(Y) > ymax else ymax
-
-print(xmin, xmax, ymin, ymax)
-
-M = [[0 for i in range(xmax - xmin + 1)] for j in range(ymax - ymin + 1)]
-# 0 for possible position of beacon
-# 1 for not possible position of beacon
-# 2 for signal
-# 3 for beacon
-
-for S in C :
-	M[S[1] - ymin][S[0] - xmin] = 2 #signal
-	M[S[3] - ymin][S[2] - xmin] = 3 #beacon
-	d = S[4] #distance
-	print(d)
-	for x in range(-1 * d, d + 1) :
-		for y in range(-d + abs(x), d - abs(x) + 1) :
-			px,py = S[0] + x, S[1] + y
-			if M[py - ymin][px - xmin]  == 0 :
-				M[py - ymin][px - xmin] = 1
-
-#for i in M :
- 	#print(i)
-
-#print(M[10 - ymin])
-print(sum([i == 1 for i in M[2000000 - ymin]]))
-	# for x in range(2 * d) :
-	# 	for y in range()
-	# 	if x >= xmin and x <= xmax :
+	if (L[1] == y_search and L[0] not in BS) :
+		#print("on ajoute le signal ", l)
+		BS.append(L[0])
+	if (L[3] == y_search and L[2] not in BS) :
+		#print("on ajoute le beacon ", l)
+		BS.append(L[2])
+	d = abs(L[0] - L[2]) + abs(L[1] - L[3]) #distance
+	if -d + abs(L[1] - y_search) < d - abs(L[1] - y_search) + 1 :
+		P.append([L[0] - d + abs(L[1] - y_search), L[0] + d - abs(L[1] - y_search)])
 
 
+P.sort(key=lambda x : x[0])
 
-# for i in M :
-# 	print(i)
+total = 0
+current_xmin = P[0][0]
+while len(P) != 0 :
+	current_elem = P[0]
+	total+= current_elem[1] - current_xmin + 1
+	current_xmin = current_elem[1] + 1
+	while len(P) != 0 :
+		e = P[0]
+		if e[1] < current_xmin :
+			P.remove(e)
+		else :
+			break
+
+print(total - len(BS))
