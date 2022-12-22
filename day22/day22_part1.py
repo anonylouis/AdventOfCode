e3793149ddc7d4fb52ff45f0e69f1698a38e94ef
Louis CALVIE <lcalvie@student.42.fr>
2022-12-22 20:54:39 +0100

    day22_part1

--- day22/day22_part1.py
+++ day22/day22_part1.py
@@ -15,25 +15,11 @@ for l in f :
 
 path = f.readline()
 
-print(pos)
-print(path)
-
-def print_map(M, pos) :
-	for k in range(len(M)) :
-		for l in range(len(M[k])) :
-			if k==pos[0] and l==pos[1] :
-				print("A", end="")
-			else :
-				print(M[k][l], end="")
-		print()
-	print()
-
 current = ""
 for i in path :
 	if i != "R" and i != "L" and i != "\n":
 		current+=i
 	else :
-		print(current, facing)
 		current = int(current)
 		for j in range(current) :
 			if facing == 0 : # >>
@@ -44,7 +30,7 @@ for i in path :
 			elif facing == 1 : # vv
 				if len(M) == pos[0] + 1 or len(M[pos[0] + 1]) <= pos[1] or M[pos[0] + 1][pos[1]] == ' ':
 					save = pos[0]
-					while (pos[0] > 0 and len(M[pos[0] - 1]) >= pos[1] and  M[pos[0] - 1][pos[1]] != ' '):
+					while (pos[0] > 0 and len(M[pos[0] - 1]) > pos[1] and  M[pos[0] - 1][pos[1]] != ' '):
 						pos[0] -= 1
 					if M[pos[0]][pos[1]] == '#' :
 						pos[0] = save
@@ -54,14 +40,14 @@ for i in path :
 				elif M[pos[0] + 1][pos[1]] == '.' :
 					pos[0] += 1
 			elif facing == 2 : # <<
-				if pos[1] == 0 or M[pos[0]][pos[1]] == ' ':
+				if pos[1] == 0 or M[pos[0]][pos[1] - 1] == ' ':
 					pos[1] = M[pos[0]].rfind('.')
 				elif M[pos[0]][pos[1] - 1] == '.' :
 					pos[1] -= 1
 			else : # ^^
 				if pos[0] == 0 or len(M[pos[0] - 1]) <= pos[1] or M[pos[0] - 1][pos[1]] == ' ':
 					save = pos[0]
-					while (pos[0] > len(M) and len(M[pos[0] + 1]) >= pos[1] and  M[pos[0] + 1][pos[1]] != ' '):
+					while ((pos[0] + 1) < len(M) and len(M[pos[0] + 1]) > pos[1] and  M[pos[0] + 1][pos[1]] != ' ') :
 						pos[0] += 1
 					if M[pos[0]][pos[1]] == '#' :
 						pos[0] = save
@@ -69,17 +55,11 @@ for i in path :
 						while (M[pos[0]][pos[1]] != '.') :
 							pos[0] -=1
 				elif M[pos[0] - 1][pos[1]] == '.' :
-					pos[1] -= 1
-			if (current == 9 and facing== 3):
-				print_map(M, pos)
-			#print_map(M, pos)
-
+					pos[0] -= 1
 		if (i == "R") :
 			facing = (facing + 1) % 4
 		elif (i =="L") :
 			facing = (facing - 1) % 4
 		current=""
 
-print(pos)
-	
 print(1000 * (pos[0] + 1) + 4 * (pos[1] + 1) + facing)
