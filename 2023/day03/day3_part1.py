d3ff9b6c74ec5b342880d6414fb76386e7ba5737
Louis <50452785+anonylouis@users.noreply.github.com>
2023-12-03 21:15:33 +0100

    Update day3_part1.py

--- 2023/day03/day3_part1.py
+++ 2023/day03/day3_part1.py
@@ -4,17 +4,29 @@ f = open('input.txt')
 
 lines = []
 for line in f :
-	lines.append(line)
+	lines.append(line[:-1])
 
-i = 0
-while i < range(len(lines)) :
+i, s = 0, 0
+while i < len(lines) :
 	j = 0
-	while j < range(len(lines[i])) :
+	while j < len(lines[i]) :
 		if (lines[i][j]).isdigit() :
 			number = ""
-			while(lines[i][j]).isdigit()) :
+			while (j < len(lines[i]) and (lines[i][j]).isdigit()) :
 				number += lines[i][j]
 				j+=1
-			
+			border = ""
+			if (i != 0) :
+				border += lines[i - 1][max(0, j - len(number) - 1) : min(len(lines[i - 1]), j + 1)]
+			if (i != (len(lines) - 1)) :
+				border += lines[i + 1][max(0, j - len(number) - 1) : min(len(lines[i + 1]), j + 1)]
+			if (j - len(number) != 0) :
+				border += lines[i][j - len(number) - 1]
+			if (j < len(lines[i])) :
+				border += lines[i][j]
+			if (any([not k.isdigit() and k != '.' for k in border])) :
+				s += int(number)
 		else :
 			j+=1
+	i += 1
+print(s)
