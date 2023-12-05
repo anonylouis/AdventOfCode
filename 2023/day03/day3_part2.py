97c21bd5835a6993f6ef4a136a78a71fef9227a0
anonylouis <louis.calvie@gmail.com>
2023-12-05 22:45:11 +0100

    [2023] day3

--- 2023/day03/day3_part2.py
+++ 2023/day03/day3_part2.py
@@ -11,9 +11,8 @@ while i < len(lines) :
 	j = 0
 	while j < len(lines[i]) :
 		if lines[i][j] == '*':
-			N = []
+			border = ""
 			if (i != 0) :
-				border = ""
 				border_min, border_max = max(0, j - 1), min(len(lines[i - 1]) - 1, j + 1)
 				if (border_min > 0 and lines[i - 1][border_min].isdigit()) :
 					while(border_min > 0 and lines[i - 1][border_min - 1].isdigit()) :
@@ -21,11 +20,8 @@ while i < len(lines) :
 				if (border_max < (len(lines[i - 1]) - 1) and lines[i - 1][border_max].isdigit()) :
 					while(border_max < (len(lines[i - 1]) - 1) and lines[i - 1][border_max + 1].isdigit()) :
 						border_max += 1
-				border += lines[i - 1][border_min : border_max + 1]
-				border = ''.join([k if k.isdigit() else ' ' for k in border])
-				N += list(map(int, border.split()))
+				border += lines[i - 1][border_min : border_max + 1] + " "
 			if (i != (len(lines) - 1)) :
-				border = ""
 				border_min, border_max = max(0, j - 1), min(len(lines[i + 1]) - 1, j + 1)
 				if (border_min > 0 and lines[i + 1][border_min].isdigit()) :
 					while(border_min > 0 and lines[i + 1][border_min - 1].isdigit()) :
@@ -33,27 +29,21 @@ while i < len(lines) :
 				if (border_max < (len(lines[i + 1]) - 1) and lines[i + 1][border_max].isdigit()) :
 					while(border_max < (len(lines[i + 1]) - 1) and lines[i + 1][border_max + 1].isdigit()) :
 						border_max += 1
-				border += lines[i + 1][border_min : border_max + 1]
-				border = ''.join([k if k.isdigit() else ' ' for k in border])
-				N += list(map(int, border.split()))
+				border += lines[i + 1][border_min : border_max + 1] + " "
 			if (j != 0) :
-				border = ""
 				border_min = j - 1
 				if (border_min > 0 and lines[i][border_min].isdigit()) :
 					while(border_min > 0 and lines[i][border_min - 1].isdigit()) :
 						border_min -= 1
-				border += lines[i][border_min : j]
-				border = ''.join([k if k.isdigit() else ' ' for k in border])
-				N += list(map(int, border.split()))
+				border += lines[i][border_min : j] + " "
 			if (j < len(lines[i])) :
-				border = ""
 				border_max = j + 1
 				if (border_max < len(lines[i]) and lines[i][border_max].isdigit()) :
 					while(border_max < (len(lines[i]) - 1) and lines[i][border_max + 1].isdigit()) :
 						border_max += 1
 				border += lines[i][j + 1 : border_max + 1]
-				border = ''.join([k if k.isdigit() else ' ' for k in border])
-				N += list(map(int, border.split()))
+			border = ''.join([k if k.isdigit() else ' ' for k in border])
+			N = list(map(int, border.split()))
 			if (len(N) == 2) :
 				s += N[0] * N[1]
 		j+=1
